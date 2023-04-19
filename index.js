a7156cc35beff3dc4b8ed8986f86b721f96a53ee
Sahib Musharraf <123538079+Sahib-Musharraf@users.noreply.github.com>
2023-04-19 17:42:15 +0530

    update index.js

--- index.js
+++ index.js
@@ -12,9 +12,7 @@ function updateClock(){
     if(h > 12) {
         h = h - 12;
         ampm= "pm";
-    } else {
-  var ampm = "am";
-}
+    } 
 h = h<10 ? "0" + h : h;
 m = m<10 ? "0" + m : m;
 s = s<10 ? "0" + s : s;
